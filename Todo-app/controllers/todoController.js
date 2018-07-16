0b3e53e6c49b374eb593eb8269fac4e7e583b7a6
kosay88 <kosay.faraj.88@gmail.com>
2018-07-16 18:52:09 +0200

    todo app

---
+++ Todo-app/controllers/todoController.js
@@ -0,0 +1,49 @@
+var bodyParser = require('body-parser');
+var mongoose =require('mongoose')
+
+//Connect to the DB:
+mongoose.connect('mongodb://kosay:ko1234567@ds131531.mlab.com:31531/todo',{useNewUrlParser: true});
+
+//Creat a schema - this is like a blueprint:
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+  if(err) throw err;
+  console.log(err);
+  console.log('item saved');
+});
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'coding along'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app){
+
+  app.get('/todo', function(req, res){
+    //get data from mongodb and pass it to view
+    Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    })
+  });
+
+  app.post('/todo', urlencodedParser, function(req, res){
+    //get data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(err,data){
+      if (err) throw err;
+      res.json(data);
+    })
+  });
+
+  app.delete('/todo/:item', function(req, res){
+    //delete the requested item from the mongodb
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+      if(err) throw err;
+      res.json(data);
+    });
+  });
+
+
+};
